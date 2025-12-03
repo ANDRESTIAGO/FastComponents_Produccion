46c893e16a7673cc1ae123a6e7e268108f1a4593
ANDRESTIAGO <drestiagomorales@gmail.com>
2025-12-02 23:25:45 -0500

    cambios

--- test/conftest.py
+++ test/conftest.py
@@ -1,3 +1,10 @@
+import sys
+from pathlib import Path
+
+# --- Añadir raíz del proyecto al PYTHONPATH ---
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(PROJECT_ROOT))
+
 import os
 import pandas as pd
 import pytest
