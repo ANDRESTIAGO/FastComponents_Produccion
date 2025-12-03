4a385482607a920ea468fab2265835b3eecc4e06
ANDRESTIAGO <drestiagomorales@gmail.com>
2025-12-03 01:03:42 -0500

    error

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from starlette.middleware.sessions import SessionMiddleware
-from app import home
+from app.factory import create_app
 
-app = FastAPI()
-
-# Montar carpeta static
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Middleware de sesión
-app.add_middleware(SessionMiddleware, secret_key="FastComponents2025")
-
-# Registrar router UNA sola vez
-app.include_router(home.router)
-
-# Cargar plantillas
-templates = Jinja2Templates(directory="templates")
+app = create_app()
